ba2d7720bc12d0f1144ad4963d5875ea731ecb68
psych0wh1z <ikeagudike@gmail.com>
2023-09-29 13:08:19 +0100

    Create server instance

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import router from './routes/index';
+
+const app = express();
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 5000;
+
+app.use('/', router);
+
+app.listen(port, host, () => {
+  console.log(`Server running on port ${port}`);
+});
