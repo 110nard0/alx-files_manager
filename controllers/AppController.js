30efa0394cec96d73f598db03b01711a9412dd87
psych0wh1z <ikeagudike@gmail.com>
2023-09-29 13:08:45 +0100

    Add application controller and routes

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+export const getStatus = (req, res) => {
+  const redisStatus = redisClient.isAlive();
+  const dbStatus = dbClient.isAlive();
+
+  res.statusCode = 200;
+  res.json({ redis: redisStatus, db: dbStatus });
+}
+
+export const getStats = async (req, res) => {
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+
+  res.statusCode = 200;
+  res.json({ users, files });
+}
