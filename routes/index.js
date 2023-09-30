4045f264461d62cdab4326c2085b7fdcc2c33510
psych0wh1z <ikeagudike@gmail.com>
2023-09-30 03:10:46 +0100

    Add routes for connecting, disconnecting and viewing a user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+
 import * as AppController from '../controllers/AppController';
+import * as AuthController from '../controllers/AuthController';
 import * as UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
 
 export default router;
