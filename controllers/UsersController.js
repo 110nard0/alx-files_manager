428da9dee4098d2d52a812a5813d5f68c62a0248
psych0wh1z <ikeagudike@gmail.com>
2023-09-30 02:57:14 +0100

    Add routes for connecting, disconnecting and viewing a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,10 @@
-import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 export const postNew = async (req, res) => {
   const { email, password } = req.body;
+
   if (!email) {
     return res.status(400).json({ error: 'Missing email' });
   }
@@ -11,12 +13,38 @@ export const postNew = async (req, res) => {
   }
 
   const existingUser = await dbClient.getUserByEmail(email);
+
   if (existingUser) {
     return res.status(400).json({ error: 'Already exists' });
   }
 
-  const hashedPassword = await bcrypt.hash(password, 12);
+  const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
   const newUser = await dbClient.createUser({ email, password: hashedPassword });
 
   res.status(201).json({ id: newUser._id, email: newUser.email });
 };
+
+export const getMe = async (req, res) => {
+  const token = req.headers['x-token'];
+
+  if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+
+  if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  console.log(userId);
+  const user = await dbClient.getUserById(userId);
+
+  if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  const { email, _id: id } = user;
+  res.status(200).json({ id, email });
+}
