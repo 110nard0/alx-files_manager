3c3fe71200429043ad582fdc51c9114427f2feb6
psych0wh1z <ikeagudike@gmail.com>
2023-09-30 13:23:53 +0100

    Import file controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import * as AppController from '../controllers/AppController';
 import * as AuthController from '../controllers/AuthController';
+import * as FilesController from '../controllers/FilesController';
 import * as UsersController from '../controllers/UsersController';
 
 const router = express.Router();
@@ -11,6 +12,8 @@ router.get('/stats', AppController.getStats);
 
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FilesController.postUpload);
 
 router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
