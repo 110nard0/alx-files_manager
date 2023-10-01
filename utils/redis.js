cdf389c4677601c40f9f7289d14d68c938001686
psych0wh1z <ikeagudike@gmail.com>
2023-10-01 09:32:04 +0100

    Add console message on redis connection

--- utils/redis.js
+++ utils/redis.js
@@ -4,6 +4,10 @@ class RedisClient {
   constructor() {
     this.client = redis.createClient();
 
+    this.client.on('connect', () => {
+      console.log('Redis server connected');
+    });
+
     this.client.on('error', (err) => {
       console.error('Redis connection error:', err);
     });
