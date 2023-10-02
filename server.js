b708f82b05ea0c3bd3953e1c523ffdf726cf1754
psych0wh1z <ikeagudike@gmail.com>
2023-10-02 08:33:42 +0100

    Add missing radix parameter to parseInt function

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import router from './routes/index';
 
 const app = express();
 const host = process.env.HOST || 'localhost';
-const port = parseInt(process.env.PORT) || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
 app.use(router);
