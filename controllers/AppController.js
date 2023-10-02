48890941391c7f1fe4b5cef7e18f20176bcb802f
psych0wh1z <ikeagudike@gmail.com>
2023-10-02 11:59:38 +0100

    Reset commit history to 'First API task'

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,17 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class AppController {
-  static getStatus = (req, res) => {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
+export const getStatus = (req, res) => {
+  const redisStatus = redisClient.isAlive();
+  const dbStatus = dbClient.isAlive();
 
-    res.statusCode = 200;
-    res.json({ redis: redisStatus, db: dbStatus });
-  };
+  res.statusCode = 200;
+  res.json({ redis: redisStatus, db: dbStatus });
+};
 
-  static async getStats (req, res) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-
-    res.statusCode = 200;
-    res.json({ users, files });
-  };
-}
-
-module.exports = AppController;
+export const getStats = async (req, res) => {
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+  res.statusCode = 200;
+  res.json({ users, files });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,10 @@
 import { Router } from 'express';
 
-import AppController from '../controllers/AppController';
-import * as AuthController from '../controllers/AuthController';
-import * as FilesController from '../controllers/FilesController';
-import * as UsersController from '../controllers/UsersController';
+import * as AppController from '../controllers/AppController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-
-router.route('/files')
-  .get(FilesController.getIndex)
-  .post(FilesController.postUpload);
-
-router.get('/files/:id', FilesController.getShow);
-router.get('/files/:id/data', FilesController.getFile);
-
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-router.get('/users/me', UsersController.getMe);
-router.post('/users', UsersController.postNew);
-
-// export default router;
-module.exports = router;
+export default router;
