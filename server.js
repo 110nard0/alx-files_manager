5c8e526db94b02972723b9480411813945a6b212
psych0wh1z <ikeagudike@gmail.com>
2023-10-02 08:27:31 +0100

    Fix router default route

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import router from './routes/index';
 
 const app = express();
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT) || 5000;
 
 app.use(express.json());
-app.use('/', router);
+app.use(router);
 
 app.listen(port, host, () => {
   console.log(`Server running on port ${port}`);
