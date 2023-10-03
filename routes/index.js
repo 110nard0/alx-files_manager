d0e1de965468f8cbfb7eb238653abac3e90843a7
psych0wh1z <ikeagudike@gmail.com>
2023-10-03 14:00:48 +0100

    Add users controller and route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import * as AppController from '../controllers/AppController';
+import * as UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
 
 export default router;
